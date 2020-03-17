8e84478628e874b1948aa02c8ccca4b277cf60b7
alvaro <gomezjordan85@hotmail.com>
2020-03-17 19:56:41 +0100

    Hola mundo y algo mas :)

---
+++ AprendiendoVue/01.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: [
+            {nombre: 'pera', cantidad: 10},
+            {nombre: 'manzana', cantidad: 20},
+            {nombre: 'platano', cantidad: 18},
+            {nombre: 'naranja', cantidad: 25},
+            {nombre: 'sandia', cantidad: 12},
+            {nombre: 'melon', cantidad: 19},
+        ]
+    }
+});
