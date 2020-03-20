c225b3e747c071b0a2516bae3392603acd7d749d
alvaro <gomezjordan85@hotmail.com>
2020-03-20 19:47:06 +0100

    Video 14 completado :)

--- AprendiendoVue/5- Vuex/hijo.js
+++ AprendiendoVue/5- Vuex/hijo.js
@@ -2,8 +2,20 @@ Vue.component('hijo',{
     template:
     `
 <div>
-    <button @click="$store.commit('aumentar')">+</button>
-    <p>{{$store.state.numero}}</p>
+    <button @click="aumentar">+</button>
+    <button @click="disminuir(2)">-</button>
+    <button @click="obtenerCursos">OBTENER CURSO</button>
+    <ul v-for="item of cursos">
+        <li>{{item.nombre}}</li>
+    </ul>
+    <p>{{numero}}</p>
 </div>
-`
+    `,
+    computed:{
+        ...Vuex.mapState(['numero', 'cursos']),
+    },
+    methods:{
+        ...Vuex.mapMutations(['aumentar', 'disminuir']),
+        ...Vuex.mapActions(['obtenerCursos'])
+    }
 })
