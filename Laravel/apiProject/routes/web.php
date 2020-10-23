a09ee117b630d66bf86ce4827c8a097aea311c5d
A. Gomez <55576063+AGomezJordan@users.noreply.github.com>
2020-10-23 15:31:57 +0200

    PRUEBA GIT

--- Laravel/apiProject/routes/web.php
+++ Laravel/apiProject/routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\messagesController;
 
 Route::get('/messages', [messagesController::class, 'index']);
 Route::get('/messages/{id}', [messagesController::class, 'store']);
+
+//ESTO ES UN APRUEBA
